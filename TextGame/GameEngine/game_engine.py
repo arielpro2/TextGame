3c366c6175eec33c17946f0b9ccf6f61188880fd
ariel <arielshokrany04@gmail.com>
2023-05-13 16:06:45 +0300

    engine

--- TextGame/GameEngine/game_engine.py
+++ TextGame/GameEngine/game_engine.py
@@ -1,6 +1,30 @@
+from TextGame.GameEngine.level_loader import LevelLoader
+
+MAIN_MENU_PROMPT = """
+Welcome to TextGame!
+
+Please choose an option:
+
+1) Start Game.
+2) Quit. 
+
+"""
+
+
 class Engine:
     def __init__(self):
-        pass
+        self.level_loader = LevelLoader()
+
+    def main_menu(self):
+        while True:
+            print(MAIN_MENU_PROMPT)
+            choice = input()
+
+            if choice == 1:
+                self.start_game()
+
+            if choice == 2:
+                break
 
     def start_game(self):
         pass
