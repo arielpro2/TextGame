2e5f746b25436f4a700ef8c85fe6138c37931b43
itamarmilgram <itamarmilgram@gmail.com>
2023-05-13 16:24:20 +0300

    level loader (not done)

--- TextGame/GameEngine/level_loader.py
+++ TextGame/GameEngine/level_loader.py
@@ -1,6 +1,14 @@
+from glob import glob
 class LevelLoader:
     def __init__(self, map_folder):
         self.map_folder = map_folder
+        self.maps=[]
+        for text_file in glob(map_folder+'/*'):
+            map_id = text_file[3::]
+            with open(text_file, 'r') as f:
+                self.maps.append([line.replace('\n', '') for line in f.readlines()])
 
     def load_map(self, map_id: int):
-        pass
+        for line in self.maps[map_id]:
+            print(line)
+
